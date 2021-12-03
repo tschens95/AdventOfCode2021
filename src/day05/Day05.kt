d28652f382e25153a9c9fe8e03c4b26d0fe3c997
jheinen <jens.heinen@n4.de>
2021-12-03 16:22:24 +0100

    preparation

---
+++ src/day05/Day05.kt
@@ -0,0 +1,18 @@
+package day05
+
+import readInput
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val input = readInput("03")
+    println(part1(input))
+    println(part2(input))
+}
