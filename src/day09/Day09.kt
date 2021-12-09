3842bb57c36b05bc52919594b7ac875be2b3ec72
jheinen <jens.heinen@n4.de>
2021-12-09 11:33:11 +0100

    day09 ez

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -1,147 +1,31 @@
 package day09
 
 import java.io.File
-import kotlin.math.min
 
 fun main() {
-
-    fun compareThree(a: Int, b: Int, c: Int): Boolean {
-        val min = min(a, min(b, c))
-        return a == min
-    }
-
-    fun compareFour(a: Int, b: Int, c: Int, d: Int): Boolean {
-        val min = min(min(a, b), min(c, d))
-        return a == min
-    }
-
-    fun compareFive(a: Int, b: Int, c: Int, d: Int, e: Int): Boolean {
-        val min = min(min(min(a, b), min(c, d)), e)
-        return a == min
-    }
 
     fun part1(input: List<String>, part1: Boolean): Int {
         val matrix = input.map { inputPos ->
             inputPos.map { it.digitToInt() }
         }
-        val lowPoints = mutableListOf<Int>()
         val rowSize = input[0].length
+        var res = 0
         for (row in input.indices) {
             for (column in 0 until rowSize) {
-                val res: Int = matrix[row][column] + 1
-                when (row) {
-                    0 -> {
-                        when (column) {
-                            0 -> {
-                                if (compareThree(
-                                        matrix[row][column],
-                                        matrix[row][column + 1],
-                                        matrix[row + 1][column]
-                                    )
-                                ) {
-                                    lowPoints.add(res)
-                                }
-                            }
-                            rowSize - 1 -> {
-                                if (compareThree(
-                                        matrix[row][column],
-                                        matrix[row][column - 1],
-                                        matrix[row + 1][column]
-                                    )
-                                ) {
-                                    lowPoints.add(res)
-                                }
-                            }
-                            else -> {
-                                if (compareFour(
-                                        matrix[row][column],
-                                        matrix[row][column + 1],
-                                        matrix[row][column - 1],
-                                        matrix[row + 1][column]
-                                    )
-                                ) {
-                                    lowPoints.add(res)
-                                }
-                            }
-                        }
-                    }
-                    input.size - 1 -> {
-                        when (column) {
-                            0 -> {
-                                if (compareThree(
-                                        matrix[row][column],
-                                        matrix[row][column + 1],
-                                        matrix[row - 1][column]
-                                    )
-                                ) {
-                                    lowPoints.add(res)
-                                }
-                            }
-                            rowSize - 1 -> {
-                                if (compareThree(
-                                        matrix[row][column],
-                                        matrix[row][column - 1],
-                                        matrix[row - 1][column]
-                                    )
-                                ) {
-                                    lowPoints.add(res)
-                                }
-                            }
-                            else -> {
-                                if (compareFour(
-                                        matrix[row][column],
-                                        matrix[row][column + 1],
-                                        matrix[row][column - 1],
-                                        matrix[row - 1][column]
-                                    )
-                                ) {
-                                    lowPoints.add(res)
-                                }
-                            }
-                        }
-                    }
-                    else -> {
-                        when (column) {
-                            0 -> {
-                                if (compareFour(
-                                        matrix[row][column],
-                                        matrix[row][column + 1],
-                                        matrix[row - 1][column],
-                                        matrix[row + 1][column]
-                                    )
-                                ) {
-                                    lowPoints.add(res)
-                                }
-                            }
-                            rowSize - 1 -> {
-                                if (compareFour(
-                                        matrix[row][column],
-                                        matrix[row][column - 1],
-                                        matrix[row - 1][column],
-                                        matrix[row + 1][column]
-                                    )
-                                ) {
-                                    lowPoints.add(res)
-                                }
-                            }
-                            else -> {
-                                if (compareFive(
-                                        matrix[row][column],
-                                        matrix[row][column + 1],
-                                        matrix[row][column - 1],
-                                        matrix[row - 1][column],
-                                        matrix[row + 1][column]
-                                    )) {
-                                    lowPoints.add(res)
-                                }
-                            }
-                        }
-                    }
+                val currVal = matrix[row][column]
+                val adjacents = listOfNotNull(
+                    matrix[row].getOrNull(column-1),
+                    matrix[row].getOrNull(column+1),
+                    matrix.getOrNull(row+1)?.get(column),
+                    matrix.getOrNull(row-1)?.get(column)
+                )
+                val minValue = adjacents.minOrNull()
+                if (currVal < minValue!!) {
+                   res += 1 + currVal
                 }
             }
         }
-        val sum = lowPoints.filter { it >= 0 }
-        return sum.sum()
+        return res
     }
 
     fun part2(input: List<String>): Int {
